a365521fbfe56b5b7ef8789352a42458291e88a4
Maharaj Haider <mhaider0@students.cs.ubc.ca>
2026-02-17 17:06:46 -0800

    init mcp server

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+// Create server instance
+const server = new McpServer({
+  name: "FeatherJSMCP",
+  version: "1.0.0",
+});
